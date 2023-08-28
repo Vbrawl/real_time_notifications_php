5dcc9bdf37f727f14523275d032aece836e17fb8
Vbrawl <greninjagr@gmail.com>
2023-08-28 13:10:51 +0300

    Added main entry to load the whole plugin.

---
+++ main.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once('relocation.php');
+
+require_once(DATABASE_ADAPTER_PATH.'/main.php');
+require_once(REAL_TIME_NOTIFICATIONS_PATH.'/src/database.php');
+require_once(REAL_TIME_NOTIFICATIONS_PATH.'/src/notification.php');
+require_once(REAL_TIME_NOTIFICATIONS_PATH.'/src/result.php');
